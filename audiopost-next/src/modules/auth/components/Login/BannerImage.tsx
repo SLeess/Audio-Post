ee21b9406ea647b5a5f08dbe772bb426738cf21e
SLeess <duraesleandro21@gmail.com>
2026-03-15 22:13:10 -0300

    feat(AudioPost - Next | Login): ✨ Add BannerImage component for login page

---
+++ audiopost-next/src/modules/auth/components/Login/BannerImage.tsx
@@ -0,0 +1,7 @@
+export default function BannerImage(){
+    return (
+        <aside className="col-span-6 bottom-0 bg-gradient-to-b from-[#16032c] to-[#0a0215] text-center m-1 rounded-lg overflow-hidden">
+            <img src="/assets/logo_login_image.png" alt="Podcasts" className="object-cover h-full" />
+        </aside>
+    );
+};
